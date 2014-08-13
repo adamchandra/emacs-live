4092d916e4e24f11cd918f14d723a6ca1a27513d
Adam Saunders <saunders@cs.umass.edu>
2014-08-13 10:00:58 -0400

    new packages

---
+++ elpa/google-20140330.856/google-autoloads.el
@@ -0,0 +1,18 @@
+;;; google-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("google-pkg.el" "google.el") (21482 30108
+;;;;;;  357473 232000))
+
+;;;***
+
+(provide 'google-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; google-autoloads.el ends here
