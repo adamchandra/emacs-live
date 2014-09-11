cb8dc6833918549bc56f492b61614c385215fd6d
Adam Saunders <saunders@cs.umass.edu>
2014-09-11 11:16:08 -0400

    package changes

---
+++ elpa/evil-jumper-20140806.724/evil-jumper-autoloads.el
@@ -0,0 +1,18 @@
+;;; evil-jumper-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("evil-jumper-pkg.el" "evil-jumper.el")
+;;;;;;  (21517 48342 420615 234000))
+
+;;;***
+
+(provide 'evil-jumper-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-jumper-autoloads.el ends here
