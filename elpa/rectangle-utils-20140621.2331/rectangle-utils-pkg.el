26dc433bd70c9cd920d3216ab5d9686c914d2300
Adam Saunders <saunders@cs.umass.edu>
2014-09-19 10:32:00 -0400

    elpa

---
+++ elpa/rectangle-utils-20140621.2331/rectangle-utils-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rectangle-utils" "20140621.2331" "Some useful rectangle functions." (quote ((emacs "24") (cl-lib "0.5"))))
---
+++ elpa/wc-mode-20120531.1344/wc-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; wc-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("wc-mode-pkg.el" "wc-mode.el") (21521
+;;;;;;  54685 955202 248000))
+
+;;;***
+
+(provide 'wc-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wc-mode-autoloads.el ends here
